d3fe73f5a5fa6e8a22919315261d4077d221d2d5
razakhan12 <raza.bb10@gmail.com>
2020-02-27 11:42:53 -0500

    Updates

---
+++ precache-manifest.a1be2255e4b84ba8892d7ad8f53cc873.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d4b5f8173950f931a5d35e14fb82ec",
+    "url": "/react-sorting/index.html"
+  },
+  {
+    "revision": "c3412672c88022f070df",
+    "url": "/react-sorting/static/css/main.d9672951.chunk.css"
+  },
+  {
+    "revision": "d85aac148bc3cfe01ac4",
+    "url": "/react-sorting/static/js/2.65443e95.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-sorting/static/js/2.65443e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3412672c88022f070df",
+    "url": "/react-sorting/static/js/main.67af731f.chunk.js"
+  },
+  {
+    "revision": "dac86a545dc110c762e5",
+    "url": "/react-sorting/static/js/runtime-main.a4817a07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sorting/static/media/logo.5d5d9eef.svg"
+  }
+]);
